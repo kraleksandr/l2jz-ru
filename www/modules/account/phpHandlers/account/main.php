0a5de27bc64a32e5a58942675260f903950e00d9
kraleksandr <kraleksandr@gmail.com>
2011-05-30 09:47:59 +0400

    Убрано поле email

--- www/modules/account/phpHandlers/account/main.php
+++ www/modules/account/phpHandlers/account/main.php
@@ -1,5 +1,5 @@
 <?php
 $sql->saveRow("
-	SELECT login,lastactive,access_level,lastIP,email FROM accounts WHERE login='".$login."'
+	SELECT login,lastactive,access_level,lastIP FROM accounts WHERE login='".$login."'
 ","LS");
 ?>
